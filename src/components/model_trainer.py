2bcdee0d47635e712e4fe9341d8703cbe0800c49
Akarsh <rakarsh456@gmail.com>
2026-02-12 14:11:44 +0530

    HyperparameterTuning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,9 +44,46 @@ class ModelTrainer:
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False)
             }
+            params={
+                "Gradient Boosting":{
+                    'learning_rate':[0.01,0.05,0.1],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[50,100,200]
+
+                },
+                "Adaboost Regressor":{
+                    'learning_rate':[0.1,0.01,0.5,0.001],
+                    'n_estimators':[50,100,200]
+
+                },
+                "K-Neighbors Regressor":{
+                    'n_neighbors':[5,7,9,11]
+                },
+                "Linear Regression":{
+
+                },
+                "Random Forest":{
+                    'n_estimators':[50,100,200]
+
+                },
+                "Decision Tree":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
+                },
+                "XGBRegressor":{
+                    'learning_rate':[0.1,0.01,0.5,0.001],
+                    'n_estimators':[50,100,200]
+
+                },
+                "CatBoosting Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[50,100,200]
+
+                }
+            }
 
             model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
-                                             models=models)
+                                             models=models, params=params)
 
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -21,7 +22,7 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
@@ -29,6 +30,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
 
             model = list(models.values())[i]
 
+            param=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)

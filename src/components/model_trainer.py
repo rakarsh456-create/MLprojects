71e0d38e8026a059f4a29278d4106d2bee7aaa70
Akarsh <rakarsh456@gmail.com>
2026-02-12 13:00:43 +0530

    Model Training Done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -10,7 +10,7 @@ from sklearn.ensemble import (
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.trees import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
 from src.exception import CustomException
@@ -21,31 +21,31 @@ from src.utils import save_object, evaluate_models
 class ModelTrainerConfig:
     trained_model_file_path = os.path.join('artifacts','model.pkl')
 
-class Modeltrainer:
+class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Splitting training and test input data.")
             X_train, y_train, X_test, y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
-                test_aaray[:,-1]
+                test_array[:,-1]
             )
             models = {
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Adaboost Regressor": AdaBoostRegressor(),
                 "K-Neighbors Regressor": KNeighborsRegressor(),
-                "Linear Regression": LinearRegRession(),
-                "Random Forst": RandomForestRegressor(),
+                "Linear Regression": LinearRegression(),
+                "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False)
             }
 
-            model_report:dict=evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
+            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
                                              models=models)
 
             ## To get best model score from dict
@@ -68,8 +68,8 @@ class Modeltrainer:
 
             predicted=best_model.predict(X_test)
 
-            r2_score = r2_score(y_test, predicted)
-            return r2_score
+            final_r2_score = r2_score(y_test, predicted)
+            return final_r2_score
 
         except Exception as e:
             raise CustomException(e, sys)

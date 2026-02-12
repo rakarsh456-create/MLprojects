0e679c873906ca5f3bd3aba2bda5a0b543892ab0
Akarsh <rakarsh456@gmail.com>
2026-02-12 18:54:16 +0530

    Completed Project

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,14 @@
 import os
 import sys
-import dill
-import numpy as np
+
+import numpy as np 
 import pandas as pd
-from src.exception import CustomException
-from src.logger import logging
+import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
+from src.exception import CustomException
 
 def save_object(file_path, obj):
     try:
@@ -16,27 +17,26 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
-
-
-def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
-
             model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
 
-            param=params[list(models.keys())[i]]
-
-            gs = GridSearchCV(model,param,cv=3)
-            gs.fit(X_train, y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train, y_train)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -51,4 +51,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
         return report
 
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
